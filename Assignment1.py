6e77e6b1810e113028d638deebda72b579234151
BombInk <62799527+BombInk@users.noreply.github.com>
2020-04-04 11:33:31 -0400

    Update Assignment1.py
    it works!

--- Assignment1.py
+++ Assignment1.py
@@ -3,18 +3,23 @@
 
 import argparse
 
-def Fib(n):
+def Fib(n, f = None):
+    r = 0
     if n == 1:
-        return 0
-    if n == 2:
-        return 1
+        r = 0
+    elif n == 2:
+        r = 1
     else:
-        return Fib(n - 1) + Fib(n - 2)
+        r = (Fib(n - 1, f) + Fib(n - 2))
+    if f != None:
+        f.write(str(r) + ' ')
+    return r
 
 
 
 
 def main():
+    print("Enter Main")
     # Create argparse
     parser = argparse.ArgumentParser(description="Computes to the nth term of the Fibonacci sequence")
     # Add an argument
@@ -22,11 +27,15 @@ def main():
     parser.add_argument("-o", "--output", action="store_true")
     # Assign the argument to a variable
     args = parser.parse_args()
+    print("Args: ", args)
     # Call the fib function and assign the return value to a variable
     result = Fib((args.num))
+    print("Calculation finished: ", result)
     if args.output:
+        print("Open file to save")
         f = open("Fibonacci.txt", "a")
-        f.write(str(result) + '\n')
+        print("File is open")
+        Fib(args.num, f)
 
 if __name__ == "__main__":
     main()
