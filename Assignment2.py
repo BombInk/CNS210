fcf95303e65ce3546f6edf2e370d698c0050b63c
BombInk <62799527+BombInk@users.noreply.github.com>
2020-04-18 16:54:12 -0400

    Add files via upload

---
+++ Assignment2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import requests
+from bs4 import BeautifulSoup
+import urllib
+
+
+result = requests.get('https://python.org/downloads')
+src = result.content
+soup = BeautifulSoup(src, 'html.parser')
+all_versions = soup.select('.list-row-container li')
+for version in all_versions:
+    if "April 6, 2013" in version.select(".release-date")[0].get_text():
+        version_number = str(version.select(".release-number")[0].get_text()).split(" ")[1]
+        if "2.7" in version_number:
+            urllib.urlretrieve("https://www.python.org/ftp/python/" + version_number + "/python-" + \
+                               version_number + ".amd64.msi", 'briston_python_version_274.msi')
