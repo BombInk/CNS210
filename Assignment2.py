19ef99040862c2e0ed71430eddaa204f49d82591
BombInk <62799527+BombInk@users.noreply.github.com>
2020-04-25 14:18:51 -0400

    Add files via upload

--- Assignment2.py
+++ Assignment2.py
@@ -9,8 +9,10 @@ src = result.content
 soup = BeautifulSoup(src, 'html.parser')
 all_versions = soup.select('.list-row-container li')
 for version in all_versions:
-    if "April 6, 2013" in version.select(".release-date")[0].get_text():
-        version_number = str(version.select(".release-number")[0].get_text()).split(" ")[1]
-        if "2.7" in version_number:
-            urllib.urlretrieve("https://www.python.org/ftp/python/" + version_number + "/python-" + \
-                               version_number + ".amd64.msi", 'briston_python_version_274.msi')
+    for i in version.select(".release-date"):
+        variablename = (i.get_text())
+        if "April 6, 2013" in version.select(".release-date")[0].get_text():
+            version_number = str(version.select(".release-number")[0].get_text()).split(" ")[1]
+            if "2.7" in version_number:
+                    urllib.urlretrieve("https://www.python.org/ftp/python/" + version_number + "/python-" + \
+                        version_number + ".amd64.msi", 'briston_python_version_274.msi')
